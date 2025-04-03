815713ec762bec88afba5b8296bc5a3b913d39cc
Keilor Rodriguez Campos <krodriguezcampos@vlacademy.com>
2025-04-02 20:29:06 -0600

    Se realiza práctica de un componente

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+//Consideraciones generales de React 
+//#1 components son funciones 
+//#2 se debe retornar un solo elemento dentro de parentesis 
+// 
 function App() {
+
+  //Este espacio es de javascript 
+
   return (
+    //Este espacio en para escribir HTML
+    //ClassName se usa para definir clases en JSX en lugar de usar Class como en Vue 
     <div className="App">
       <header className="App-header">
+
+        {/* Las llaves se usan como sustitutos al v-bind de Vue  */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +28,30 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
 
+
+//Práctica de un componente
+
+function HelloWorld() {
+  let nombre = "Hola Mundo"; //Se pueden definir variables como en javascript
+  let parrafo = "Este es un componente de React"; //Se pueden definir variables como en javascript
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> {nombre} </h1>
+        <p> {parrafo} </p>
+        <p>  lorem ipsum  </p>
+      </header>
+    </div>
+  );
+}
+
+
 export default App;
+
+export { HelloWorld };
